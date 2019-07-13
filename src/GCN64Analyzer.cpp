e439bccedba8ab159d7bfeee7930ffed76c392e5
Justin S <Ownasaurus@gmail.com>
2019-07-12 20:36:40 -0400

    bulk of protocol code added but incomplete

--- src/GCN64Analyzer.cpp
+++ src/GCN64Analyzer.cpp
@@ -32,7 +32,67 @@ void GCN64Analyzer::WorkerThread()
 	if( mGCN64Data->GetBitState() == BIT_LOW )
 		mGCN64Data->AdvanceToNextEdge();
 	
-	// TODO: do all of the anaylsis
+	// get to the first falling edge
+	mGCN64Data->AdvanceToNextEdge();
+
+	U64 starting_sample = mGCN64Data->GetSampleNumber();
+
+	// get middle of first pulse, 2us later
+	// TODO: USE CLOCK GENERATOR TO DO THIS?
+
+	U32 command = mGCN64Data->GetBitState();
+	U8 bits_read = 1;
+	U8 currentBit = 0;
+
+	while (1) // read at least 9 bits (1 byte + stop bit)
+	{
+		command = command << 1; // make room for the new bit
+
+		// make sure we start at a rising edge
+		if (mGCN64Data->GetBitState() == BIT_LOW)
+			mGCN64Data->AdvanceToNextEdge();
+
+		// get to the falling edge
+		mGCN64Data->AdvanceToNextEdge();
+
+		// get middle of first pulse, 2us later
+		// TODO: USE CLOCK GENERATOR TO DO THIS?
+
+		currentBit = mGCN64Data->GetBitState();
+
+		if (currentBit == 5) // timeout TODO: replace this with actual determination if we're at the end of the data
+		{
+			if (bits_read >= 8)
+			{
+				command = command >> 2; // get rid of the stop bit AND the room we made for an additional bit
+				break;
+			}
+			else // there is no possible way this can be a real command
+			{
+				command = 5; // dummy value
+				break;
+			}
+		}
+		command += currentBit;
+
+		bits_read++;
+
+		if (bits_read >= 25) // this is the longest known command length
+		{
+			command = command >> 1; // get rid of the stop bit (which is always a 1)
+			break;
+		}
+
+		Frame frame;
+		frame.mData1 = command;
+		frame.mFlags = 0;
+		frame.mStartingSampleInclusive = starting_sample;
+		frame.mEndingSampleInclusive = mGCN64Data->GetSampleNumber();
+
+		mResults->AddFrame(frame);
+		mResults->CommitResults();
+		ReportProgress(frame.mEndingSampleInclusive);
+	}
 
 	/*
 	U32 samples_per_bit = mSampleRateHz / mSettings->mBitRate;
